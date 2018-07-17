bee2bda4105adceb74d54d91d230cfdcc4e7ef53
unknown <leonlau95@gmail.com>
2018-07-17 10:48:03 +0800

    first commit

---
+++ weatherfilemgr.js
@@ -0,0 +1,74 @@
+const {MongoClient} = require('mongodb');
+const fs = MongoClient;
+
+//const database = 'mongodb://localhost:27017';
+//const database = 'mongodb://lab123:lab123@ds245250.mlab.com:45250/weatherapp';
+const database = 'mongodb://project:project12@ds239681.mlab.com:39681/304cem'
+
+const saveData = (newData) => {
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(database, {useNewUrlParser: true}, (err, client) => {
+      if (err) {
+        reject('Unable to connect to MongoDB');
+      }
+      console.log('Connected to MongoDB');
+      const db = client.db('304cem');
+
+      db.collection('WeatherCollection').insertOne(newData, (err, result) => {
+        if (err) {
+          reject('Unable to insert');
+        }
+        resolve(result);
+      })
+
+      client.close();
+    });
+  });
+};
+
+const getAllData = () => {
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(database, {useNewUrlParser: true}, (err, client) => {
+      if (err) {
+        reject('Unable to connect to MongoDB');
+      }
+      console.log('Connected to MongoDB');
+      const db = client.db('304cem');
+
+      db.collection('WeatherCollection').find().toArray().then( (docs) => {
+        resolve(docs);
+      }, (err) => {
+        reject('Unable to fetch docs');
+      });
+
+      client.close();
+    });
+  });
+};
+
+const deleteAll = () => {
+  return new Promise((resolve, reject) => {
+    MongoClient.connect(database, {useNewUrlParser: true}, (err, client) => {
+      if (err) {
+        reject('Unable to connect to MongoDB');
+      }
+      console.log('Connected to MongoDB');
+      const db = client.db('304cem');
+
+      db.collection('WeatherCollection').remove({}).then( (result) => {
+        resolve(result);
+      }, (err) => {
+        reject('Unable to delete');
+      });
+
+      client.close();
+    });
+  });
+};
+
+
+module.exports = {
+  saveData,
+  getAllData,
+  deleteAll,
+}
